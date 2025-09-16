257a1b3d8452cde5141d6fb135ddf5c4767f7866
zak-djahed <156936747+zak-djahed@users.noreply.github.com>
2025-09-16 16:18:11 -0400

    Update vite.config.ts to contain base

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: '/zakdjahed/',
   server: {
     host: "::",
     port: 8080,
